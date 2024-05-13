93912a2f14758c1cc5a474ed6400857955489b42
Jeff Glass <glass.jeffrey@gmail.com>
2024-05-12 22:31:26 -0500

    Fix pre-commit isssues, remove unused file

--- sphinx_pyscript/__init__.py
+++ sphinx_pyscript/__init__.py
@@ -13,7 +13,6 @@ from sphinx.util.docutils import SphinxDirective
 from sphinx.util.fileutil import copy_asset_file
 from sphinx.util.logging import getLogger
 
-
 DEFAULT_VERSION = "2024.4.2"
 
 
@@ -23,7 +22,9 @@ def setup(app: Sphinx):
         "pyscript_js", f"https://pyscript.net/releases/{DEFAULT_VERSION}/core.js", "env"
     )
     app.add_config_value(
-        "pyscript_css", f"https://pyscript.net/releases/{DEFAULT_VERSION}/core.css", "env"
+        "pyscript_css",
+        f"https://pyscript.net/releases/{DEFAULT_VERSION}/core.css",
+        "env",
     )
     app.add_directive("py-config", PyConfig)
     app.add_directive("py-script", PyScript)
@@ -31,7 +32,7 @@ def setup(app: Sphinx):
     app.add_directive("py-terminal", PyTerminal)
     app.connect("doctree-read", doctree_read)
     app.connect("html-page-context", add_html_context)
-    app.connect('build-finished', copy_asset_files)
+    app.connect("build-finished", copy_asset_files)
     return {"version": __version__, "parallel_read_safe": True}
 
 
@@ -77,7 +78,9 @@ class PyScript(SphinxDirective):
             code = "\n".join(self.content)
         else:
             raise self.error("Must provide either content or the 'file' option")
-        return [nodes.raw("", f"<script type='py'>\n{code}\n</script>\n", format="html")]
+        return [
+            nodes.raw("", f"<script type='py'>\n{code}\n</script>\n", format="html")
+        ]
 
 
 class PyEditor(SphinxDirective):
@@ -99,24 +102,33 @@ class PyEditor(SphinxDirective):
         if "config" in self.options:
             attrs += f' config="{self.options["""config"""]}"'
         if "setup" in self.options:
-            attrs += f'setup'
+            attrs += "setup"
         if self.content:
             code = "\n".join(self.content)
-        return [nodes.raw("", f"<script type='py-editor' {attrs}>\n{code}\n</script>\n", format="html")]
+        return [
+            nodes.raw(
+                "",
+                f"<script type='py-editor' {attrs}>\n{code}\n</script>\n",
+                format="html",
+            )
+        ]
 
 
 class PyTerminal(SphinxDirective):
     """Add a py-terminal tag"""
 
-    option_spec = {
-    }
+    option_spec = {}
 
     def run(self):
         """Add the py-terminal tag"""
         attrs = ""
         if "worker" in self.options:
             attrs += " worker"
-        return [nodes.raw("", f"<script type='py' terminal {attrs}></script>\n", format="html")]
+        return [
+            nodes.raw(
+                "", f"<script type='py' terminal {attrs}></script>\n", format="html"
+            )
+        ]
 
 
 def add_html_context(
@@ -151,8 +163,9 @@ def doctree_read(app: Sphinx, doctree: nodes.document):
                 )
             )
 
+
 def copy_asset_files(app, exc):
-    if app.builder.format == 'html' and not exc:
-        custom_file = (Path(__file__).parent / 'mini-coi.js').absolute()
+    if app.builder.format == "html" and not exc:
+        custom_file = (Path(__file__).parent / "mini-coi.js").absolute()
         static_dir = (Path(app.builder.outdir)).absolute()
         copy_asset_file(custom_file, static_dir)
